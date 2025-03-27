84c8e5a710dea18ce701b46675ab374904a4d9a7
estevao-santos-ribeiro <estevaosantos@uniplaclages.edu.br>
2025-03-27 15:50:50 -0300

    Implementada rotação do robô

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -8,25 +8,40 @@
             int posicaoX = 1;
             int posicaoY = 1;
             char direcao = 'N';
-            string comandos = "MMM";
+            string comandos = "MMMD";
             char[] arrayComandos = comandos.ToCharArray();
 
             Console.WriteLine($"posição inicial: {posicaoX} {posicaoY} {direcao}");
-            
-            for(int elemento = 0; elemento < arrayComandos.Length; elemento++)
+
+            for (int elemento = 0; elemento < arrayComandos.Length; elemento++)
             {
                 char comando = arrayComandos[elemento];
-                
-                if(comando == 'M' && direcao == 'N')
+
+                // rotação do robô
+                switch (comando)
+                {
+                    case 'D':
+                        direcao = MudarDirecaoDireita(direcao);
+                        break;
+                    case 'E':
+                        direcao = MudarDirecaoEsquerda(direcao);
+                        break;
+                }
+
+                // movimentação para frente
+                if (comando == 'M' && direcao == 'N')
                 {
                     posicaoY += 1;
-                } else if (comando == 'M' && direcao == 'L')
+                }
+                else if (comando == 'M' && direcao == 'L')
                 {
                     posicaoX += 1;
-                } else if (comando == 'M' && direcao == 'S')
+                }
+                else if (comando == 'M' && direcao == 'S')
                 {
                     posicaoY -= 1;
-                } else if (comando == 'M' && direcao == 'O')
+                }
+                else if (comando == 'M' && direcao == 'O')
                 {
                     posicaoX -= 1;
                 }
@@ -35,6 +50,46 @@
             Console.WriteLine($"posição final: {posicaoX} {posicaoY} {direcao}");
 
             Console.ReadLine();
+        }
+
+        static char MudarDirecaoDireita(char direcao)
+        {
+            switch (direcao)
+            {
+                case 'N':
+                    direcao = 'L';
+                    break;
+                case 'L':
+                    direcao = 'S';
+                    break;
+                case 'S':
+                    direcao = 'O';
+                    break;
+                case 'O':
+                    direcao = 'N';
+                    break;
+            }
+            return direcao;
+        }
+
+        static char MudarDirecaoEsquerda(char direcao)
+        {
+            switch (direcao)
+            {
+                case 'N':
+                    direcao = 'O';
+                    break;
+                case 'L':
+                    direcao = 'N';
+                    break;
+                case 'S':
+                    direcao = 'L';
+                    break;
+                case 'O':
+                    direcao = 'S';
+                    break;
+            }
+            return direcao;
         }
     }
 }
