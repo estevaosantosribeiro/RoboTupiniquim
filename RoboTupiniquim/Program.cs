bbc7e344756af0dcca87f078f7203b28dfc6c067
estevao-santos-ribeiro <estevaosantos@uniplaclages.edu.br>
2025-03-30 17:51:14 -0300

    Implemetada movimentação do segundo robô

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             //int tamanhoGrid = 5;
+            Console.WriteLine("Movimente o primeiro robô!\n");
+            MovimentarRobo();
+            Console.WriteLine("\nMovimente o segundo robô!\n");
+            MovimentarRobo();
+
+            Console.ReadLine();
+        }
+
+        static void MovimentarRobo()
+        {
             char[] movimentos;
             string[] coordenadasIniciais;
 
@@ -19,13 +29,11 @@
             RoboTupiniquim.Movimentar(movimentos);
 
             ExibirPosicaoFinal(RoboTupiniquim.posicaoX, RoboTupiniquim.posicaoY, RoboTupiniquim.direcao);
-
-            Console.ReadLine();
         }
 
         static string[] ObterCoordenadas()
         {
-            Console.Write("Digite a posição e direção inicial: ");
+            Console.Write("Digite a posição e direção inicial do robô: ");
             string coordenadasIniciais = Console.ReadLine()!;
             string[] coordenadas = coordenadasIniciais.Split(" ");
             return coordenadas;
@@ -40,12 +48,12 @@
 
         static void ExibirPosicaoInicial(int posicaoX, int posicaoY, char direcao)
         {
-            Console.WriteLine($"posição inicial: {posicaoX} {posicaoY} {direcao}");
+            Console.WriteLine($"Posição inicial: {posicaoX} {posicaoY} {direcao}");
         }
 
         static void ExibirPosicaoFinal(int posicaoX, int posicaoY, char direcao)
         {
-            Console.WriteLine($"posição final: {posicaoX} {posicaoY} {direcao}");
+            Console.WriteLine($"Posição final: {posicaoX} {posicaoY} {direcao}");
         }
     }
 }
